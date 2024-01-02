b7bb109d9dfedc2964850bf6b071ec6ae99906da
Wael El Oraiby <wael.eloraiby@gmail.com>
2024-01-01 20:47:02 -0500

    Replace unused layout code with the new one

--- src/lib.rs
+++ src/lib.rs
@@ -495,25 +495,6 @@ impl Context {
 #[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Id(u32);
 
-#[derive(Clone, Default)]
-struct Row {
-    start: usize,
-    len: usize,
-    item_index: usize,
-}
-
-#[derive(Clone, Default)]
-struct LayoutManager {
-    pub style: Style,
-    pub last_rect: Recti,
-    pub stack: Vec<Layout>,
-    pub row_widths_stack: Vec<i32>,
-    row_stack: Vec<Row>,
-
-    pub current_row_widths: Vec<i32>,
-    pub item_index: usize,
-}
-
 #[derive(Clone)]
 pub struct Container {
     id: Id,
@@ -532,17 +513,6 @@ pub struct Container {
     pub is_root: bool,
     pub text_stack: Vec<char>,
     layout: LayoutManager,
-}
-
-#[derive(Default, Copy, Clone)]
-pub struct Layout {
-    pub body: Recti,
-    pub next: Recti,
-    pub position: Vec2i,
-    pub size: Dimensioni,
-    pub max: Vec2i,
-    pub next_row: i32,
-    pub indent: i32,
 }
 
 #[derive(Clone)]
@@ -700,160 +670,6 @@ fn hash_str(hash_0: &mut Id, s: &str) {
 fn hash_bytes(hash_0: &mut Id, s: &[u8]) {
     for c in s {
         *hash_0 = Id(hash_step(hash_0.0, *c as u32));
-    }
-}
-
-impl LayoutManager {
-    fn push_layout(&mut self, body: Recti, scroll: Vec2i) {
-        let mut layout: Layout = Layout {
-            body: Recti { x: 0, y: 0, width: 0, height: 0 },
-            next: Recti { x: 0, y: 0, width: 0, height: 0 },
-            position: Vec2i { x: 0, y: 0 },
-            size: Dimension { width: 0, height: 0 },
-            max: Vec2i { x: 0, y: 0 },
-            next_row: 0,
-            indent: 0,
-        };
-        layout.body = rect(body.x - scroll.x, body.y - scroll.y, body.width, body.height);
-        layout.max = vec2(-i32::MAX, -i32::MAX);
-        self.stack.push(layout);
-        self.row(&[0], 0);
-    }
-
-    fn top(&self) -> &Layout {
-        return self.stack.last().unwrap();
-    }
-
-    fn top_mut(&mut self) -> &mut Layout {
-        return self.stack.last_mut().unwrap();
-    }
-
-    pub fn begin_column(&mut self) {
-        let layout = self.next();
-
-        let row = Row {
-            start: self.row_stack.len(),
-            len: self.current_row_widths.len(),
-            item_index: self.item_index,
-        };
-        for i in 0..self.current_row_widths.len() {
-            self.row_widths_stack.push(self.current_row_widths[i]);
-        }
-        self.current_row_widths.clear();
-        self.item_index = 0;
-        self.row_stack.push(row);
-        self.push_layout(layout, vec2(0, 0));
-    }
-
-    pub fn end_column(&mut self) {
-        let b = self.top().clone();
-        self.stack.pop();
-        let row = self.row_stack.pop().unwrap();
-        self.current_row_widths.clear();
-        for i in 0..row.len {
-            self.current_row_widths.push(self.row_widths_stack[i + row.start]);
-        }
-        self.row_widths_stack.shrink_to(self.row_widths_stack.len() - row.len);
-        self.item_index = row.item_index;
-
-        let a = self.top_mut();
-        a.position.x = if a.position.x > b.position.x + b.body.x - a.body.x {
-            a.position.x
-        } else {
-            b.position.x + b.body.x - a.body.x
-        };
-        a.next_row = if a.next_row > b.next_row + b.body.y - a.body.y {
-            a.next_row
-        } else {
-            b.next_row + b.body.y - a.body.y
-        };
-
-        // propagate max to the "current" top of the stack (parent) layout
-        a.max.x = max(a.max.x, b.max.x);
-        a.max.y = max(a.max.y, b.max.y);
-    }
-
-    fn row_for_layout(&mut self, height: i32) {
-        let layout = self.top_mut();
-        layout.position = vec2(layout.indent, layout.next_row);
-        layout.size.height = height;
-        self.item_index = 0;
-    }
-
-    pub fn row(&mut self, widths: &[i32], height: i32) {
-        self.current_row_widths.clear();
-        for i in 0..widths.len() {
-            self.current_row_widths.push(widths[i]);
-        }
-        self.row_for_layout(height);
-    }
-
-    pub fn set_width(&mut self, width: i32) {
-        self.top_mut().size.width = width;
-    }
-
-    pub fn set_height(&mut self, height: i32) {
-        self.top_mut().size.height = height;
-    }
-
-    pub fn next(&mut self) -> Recti {
-        let dcell_size = self.style.default_cell_size;
-        let padding = self.style.padding;
-        let spacing = self.style.spacing;
-        let row_cells_count = self.current_row_widths.len();
-
-        let mut res: Recti = Recti { x: 0, y: 0, width: 0, height: 0 };
-
-        let lsize_y = self.top().size.height;
-
-        // next grid line
-        if self.item_index == row_cells_count {
-            self.row_for_layout(lsize_y);
-        }
-
-        res.x = self.top().position.x;
-        res.y = self.top().position.y;
-        res.width = if self.current_row_widths.len() > 0 {
-            self.current_row_widths[self.item_index]
-        } else {
-            self.top().size.width
-        };
-        res.height = self.top().size.height;
-
-        if res.width == 0 {
-            res.width = dcell_size.width + padding * 2;
-        }
-        if res.height == 0 {
-            res.height = dcell_size.height + padding * 2;
-        }
-        if res.width < 0 {
-            res.width += self.top().body.width - res.x + 1;
-        }
-        if res.height < 0 {
-            res.height += self.top().body.height - res.y + 1;
-        }
-
-        // ensure it will never exceeds
-        if self.item_index < row_cells_count {
-            self.item_index += 1;
-        }
-
-        ///////////
-        // update the next position/row/body/max/...
-        ////////
-        self.top_mut().position.x += res.width + spacing;
-        self.top_mut().next_row = if self.top().next_row > res.y + res.height + spacing {
-            self.top().next_row
-        } else {
-            res.y + res.height + spacing
-        };
-
-        res.x += self.top().body.x;
-        res.y += self.top().body.y;
-        self.top_mut().max.x = max(self.top().max.x, res.x + res.width);
-        self.top_mut().max.y = max(self.top().max.y, res.y + res.height);
-        self.last_rect = res;
-        return self.last_rect;
     }
 }
 
