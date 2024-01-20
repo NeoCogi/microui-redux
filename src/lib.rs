a00df8b860ec9fc295db578cfab98b665c0c654f
Wael El Oraiby <wael.eloraiby@gmail.com>
2024-01-20 09:02:14 -0500

    Split container into its own file

--- src/lib.rs
+++ src/lib.rs
@@ -54,8 +54,11 @@ use std::{f32, hash::Hash, rc::Rc};
 
 use rs_math3d::*;
 
+mod container;
 mod layout;
+
 pub use layout::*;
+pub use container::*;
 
 use bitflags::*;
 use std::cmp::{min, max};
@@ -446,56 +449,6 @@ impl Context {
 #[derive(Default, Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub struct Id(u32);
 
-#[derive(Clone)]
-pub struct Container {
-    id: Id,
-    atlas: Rc<dyn Atlas>,
-    pub style: Style,
-    pub name: String,
-    pub rect: Recti,
-    pub body: Recti,
-    pub content_size: Vec2i,
-    pub scroll: Vec2i,
-    pub zindex: i32,
-    pub open: bool,
-    pub command_list: Vec<Command>,
-    pub clip_stack: Vec<Recti>,
-    pub children: Vec<usize>,
-    pub is_root: bool,
-    pub text_stack: Vec<char>,
-    layout: LayoutManager,
-}
-
-#[derive(Clone)]
-pub enum Command {
-    Clip {
-        rect: Recti,
-    },
-    Recti {
-        rect: Recti,
-        color: Color,
-    },
-    Text {
-        font: FontId,
-        pos: Vec2i,
-        color: Color,
-        str_start: usize,
-        str_len: usize,
-    },
-    Icon {
-        rect: Recti,
-        id: Icon,
-        color: Color,
-    },
-    None,
-}
-
-impl Default for Command {
-    fn default() -> Self {
-        Command::None
-    }
-}
-
 #[derive(Default, Copy, Clone)]
 #[repr(C)]
 pub struct Color {
@@ -603,146 +556,6 @@ fn hash_str(hash_0: &mut Id, s: &str) {
 fn hash_bytes(hash_0: &mut Id, s: &[u8]) {
     for c in s {
         *hash_0 = Id(hash_step(hash_0.0, *c as u32));
-    }
-}
-
-impl Container {
-    pub fn push_clip_rect(&mut self, rect: Recti) {
-        let last = self.get_clip_rect();
-        self.clip_stack.push(rect.intersect(&last).unwrap_or_default());
-    }
-
-    pub fn pop_clip_rect(&mut self) {
-        self.clip_stack.pop();
-    }
-
-    pub fn get_clip_rect(&mut self) -> Recti {
-        match self.clip_stack.last() {
-            Some(r) => *r,
-            None => UNCLIPPED_RECT,
-        }
-    }
-
-    pub fn check_clip(&mut self, r: Recti) -> Clip {
-        let cr = self.get_clip_rect();
-        if r.x > cr.x + cr.width || r.x + r.width < cr.x || r.y > cr.y + cr.height || r.y + r.height < cr.y {
-            return Clip::All;
-        }
-        if r.x >= cr.x && r.x + r.width <= cr.x + cr.width && r.y >= cr.y && r.y + r.height <= cr.y + cr.height {
-            return Clip::None;
-        }
-        return Clip::Part;
-    }
-
-    pub fn push_command(&mut self, cmd: Command) {
-        self.command_list.push(cmd);
-    }
-
-    pub fn push_text(&mut self, str: &str) -> usize {
-        let str_start = self.text_stack.len();
-        for c in str.chars() {
-            self.text_stack.push(c);
-        }
-        return str_start;
-    }
-
-    pub fn set_clip(&mut self, rect: Recti) {
-        self.push_command(Command::Clip { rect });
-    }
-
-    pub fn draw_rect(&mut self, mut rect: Recti, color: Color) {
-        rect = rect.intersect(&self.get_clip_rect()).unwrap_or_default();
-        if rect.width > 0 && rect.height > 0 {
-            self.push_command(Command::Recti { rect, color });
-        }
-    }
-
-    pub fn draw_box(&mut self, r: Recti, color: Color) {
-        self.draw_rect(rect(r.x + 1, r.y, r.width - 2, 1), color);
-        self.draw_rect(rect(r.x + 1, r.y + r.height - 1, r.width - 2, 1), color);
-        self.draw_rect(rect(r.x, r.y, 1, r.height), color);
-        self.draw_rect(rect(r.x + r.width - 1, r.y, 1, r.height), color);
-    }
-
-    pub fn draw_text(&mut self, font: FontId, str: &str, pos: Vec2i, color: Color) {
-        let tsize = self.atlas.get_text_size(font, str);
-        let rect: Recti = rect(pos.x, pos.y, tsize.width, tsize.height);
-        let clipped = self.check_clip(rect);
-        match clipped {
-            Clip::All => return,
-            Clip::Part => {
-                let clip = self.get_clip_rect();
-                self.set_clip(clip)
-            }
-            _ => (),
-        }
-
-        let str_start = self.push_text(str);
-        self.push_command(Command::Text {
-            str_start,
-            str_len: str.len(),
-            pos,
-            color,
-            font,
-        });
-        if clipped != Clip::None {
-            self.set_clip(UNCLIPPED_RECT);
-        }
-    }
-
-    pub fn draw_icon(&mut self, id: Icon, rect: Recti, color: Color) {
-        let clipped = self.check_clip(rect);
-        match clipped {
-            Clip::All => return,
-            Clip::Part => {
-                let clip = self.get_clip_rect();
-                self.set_clip(clip)
-            }
-            _ => (),
-        }
-        self.push_command(Command::Icon { id, rect, color });
-        if clipped != Clip::None {
-            self.set_clip(UNCLIPPED_RECT);
-        }
-    }
-
-    pub fn text(&mut self, text: &str) {
-        let font = self.style.font;
-        let color = self.style.colors[ControlColor::Text as usize];
-        let h = self.atlas.get_font_height(font) as i32;
-        self.layout.begin_column();
-        self.layout.row(&[-1], h);
-
-        // lines() doesn't count line terminator
-        for line in text.lines() {
-            let mut r = self.layout.next();
-            let mut rx = r.x;
-            let words = line.split_inclusive(' ');
-            for w in words {
-                // TODO: split w when its width > w into many lines
-                let tw = self.atlas.get_text_size(font, w).width;
-                if tw + rx < r.x + r.width {
-                    self.draw_text(font, w, vec2(rx, r.y), color);
-                    rx += tw;
-                } else {
-                    r = self.layout.next();
-                    rx = r.x;
-                }
-            }
-        }
-        self.layout.end_column();
-    }
-
-    fn draw_frame(&mut self, rect: Recti, colorid: ControlColor) {
-        let color = self.style.colors[colorid as usize];
-        self.draw_rect(rect, color);
-        if colorid == ControlColor::ScrollBase || colorid == ControlColor::ScrollThumb || colorid == ControlColor::TitleBG {
-            return;
-        }
-        let border_color = self.style.colors[ControlColor::Border as usize];
-        if border_color.a != 0 {
-            self.draw_box(expand_rect(rect, 1), border_color);
-        }
     }
 }
 
